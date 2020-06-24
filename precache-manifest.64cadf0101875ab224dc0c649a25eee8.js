7f081c0fad1a3e1b427bf3e2f4ea223bc4b17f65
Ashrut <1813301@iiitdmj.ac.in>
2020-06-24 21:16:59 +0530

    Updates

---
+++ precache-manifest.64cadf0101875ab224dc0c649a25eee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d40517da07d2e9bc5bebd35de027027",
+    "url": "/prabhaics/index.html"
+  },
+  {
+    "revision": "42ce4f39460bc2903569",
+    "url": "/prabhaics/static/css/2.b437d9c2.chunk.css"
+  },
+  {
+    "revision": "fbe71f2eeb13abf46c51",
+    "url": "/prabhaics/static/css/main.68e09eca.chunk.css"
+  },
+  {
+    "revision": "42ce4f39460bc2903569",
+    "url": "/prabhaics/static/js/2.9b9193bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/prabhaics/static/js/2.9b9193bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe71f2eeb13abf46c51",
+    "url": "/prabhaics/static/js/main.1cf4049e.chunk.js"
+  },
+  {
+    "revision": "0c484802d80ad561d31f",
+    "url": "/prabhaics/static/js/runtime-main.91a97e58.js"
+  }
+]);
